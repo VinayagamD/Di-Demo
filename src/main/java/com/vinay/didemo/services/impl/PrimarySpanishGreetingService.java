5733bdadfb9aba0b18914f77087ab5694ea27888
VinayagamD <vinayganeshdxj@gmail.com>
2019-05-07 00:04:09 +0530

    Profile Intro

--- src/main/java/com/vinay/didemo/services/impl/PrimarySpanishGreetingService.java
+++ src/main/java/com/vinay/didemo/services/impl/PrimarySpanishGreetingService.java
@@ -1,16 +1,17 @@
 package com.vinay.didemo.services.impl;
 
+
 import com.vinay.didemo.services.GreetingService;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("es")
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello From Primary Greeting Service";
+        return "Servicio de Saludo Primario";
     }
 }
